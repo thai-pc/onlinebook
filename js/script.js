3e5cea11f591c33c055f63859445c01f34d74969
thai-pc <95364471+thai-pc@users.noreply.github.com>
2021-12-01 21:56:03 +0700

    Add files via upload
    web sách online

---
+++ js/script.js
@@ -0,0 +1,170 @@
+//Ẩn hiện thanh tìm kiếm
+searchForm = document.querySelector(".header .header-1 .search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+};
+//Kết thúc ẩn hiện thanh tìm kiếm
+//Ẩn hiện đăng nhập
+let loginForm = document.querySelector(".login-form-container");
+
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+};
+document.querySelector("#close-login-btn").onclick = () => {
+  loginForm.classList.remove("active");
+};
+//Kết thúc ẩn hiện đăng nhập
+//Hiện thanh navbar khi kéo cửa sổ xuống
+window.onscroll = () => {
+  searchForm.classList.remove("active");
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.remove("active");
+  }
+};
+window.onload = () => {
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.remove("active");
+  }
+  fadeOut();
+};
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut(){
+  setTimeout(loader, 4000);
+}
+//Kết thúc hiện thanh navbar khi kéo cửa sổ xuống
+
+//books-slider
+var swiper = new Swiper(".books-slider", {
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+//kết thúc books-slider
+//bắt đầu featured slider
+var swiper = new Swiper(".featured-slider", {
+  spaceBetween: 10,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+//kết thúc featured slider
+
+//bắt đầu arrivals slider
+var swiper = new Swiper(".arrivals-slider", {
+  spaceBetween: 10,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+//kết thúc arrivals slider
+//bắt đầu reviews slider
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 10,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+//kết thúc reviews slider
+//bắt đầu blogs slider
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetween: 10,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+//kết thúc blogs slider
